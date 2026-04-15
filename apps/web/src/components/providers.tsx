f1a851b1200db48096c3061875767d600046c783
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2026-04-15 21:11:02 +0200

    refactor(web): remove QueryClientProvider from Providers component

--- apps/web/src/components/providers.tsx
+++ apps/web/src/components/providers.tsx
@@ -1,19 +1,14 @@
 "use client";
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@zendak/ui/components/sonner";
 
 import { ThemeProvider } from "./theme-provider";
 
-const queryClient = new QueryClient();
-
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <QueryClientProvider client={queryClient}>
-      <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
-        {children}
-        <Toaster richColors />
-      </ThemeProvider>
-    </QueryClientProvider>
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+      {children}
+      <Toaster richColors />
+    </ThemeProvider>
   );
 }
