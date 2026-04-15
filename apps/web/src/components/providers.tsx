1ab623f4820064d05b5d775d2930cbfd6f0264f1
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2026-04-15 20:36:28 +0200

    feat(web): integrate QueryClientProvider in providers

--- apps/web/src/components/providers.tsx
+++ apps/web/src/components/providers.tsx
@@ -1,14 +1,19 @@
 "use client";
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@zendak/ui/components/sonner";
 
 import { ThemeProvider } from "./theme-provider";
 
+const queryClient = new QueryClient();
+
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
-      {children}
-      <Toaster richColors />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+        {children}
+        <Toaster richColors />
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 }
